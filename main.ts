9b80634009969c2de6a0a0c8301e8a2b5815ac8d
juliacatanz <113040686+juliacatanz@users.noreply.github.com>
2022-09-22 10:55:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    for (let x = 0; x <= 4; x++) {
+        basic.showLeds(`
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+        basic.pause(100)
+    }
+})
